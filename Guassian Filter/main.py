9ec8394c8275a29b98a0c7fd98602444431499b7
Rohit <dev.rohitnp@gmail.com>
2023-01-05 19:54:08 +0530

    Modified Gaussian Filter Function; and added .gitignore

--- Guassian Filter/main.py
+++ Guassian Filter/main.py
@@ -25,26 +25,26 @@ def GuassianBlur(img: np.ndarray, sigma: float | int, filter_shape: list | tuple
     >>> g_filter, blur_image = GuassianBlur(img, 4)
     '''
 
-    # generating filter shape with the sigma(standard deviation) given, if filter_shape is None
     if filter_shape == None:
+        # generating filter shape with the sigma(standard deviation) given
         _ = 2 * int(4 * sigma + 0.5) + 1
         filter_shape = [_, _]
 
-    size_y = filter_shape[0] // 2
-    size_x = filter_shape[1] // 2
-    gaussian_filter = np.zeros((filter_shape[0], filter_shape[1]), np.float32)
+    elif len(filter_shape) != 2:
+        raise Exception('shape of argument `filter_shape` is not a supported')
 
-    for x in range(-size_y, size_y+1):
-        for y in range(-size_x, size_x+1):
+    m, n = filter_shape
+
+    m_half = m // 2
+    n_half = n // 2
+
+    gaussian_filter = np.zeros((m, n), np.float32)
+
+    for y in range(-m_half, m_half):
+        for x in range(-n_half, n_half):
             normal = 1 / (2.0 * np.pi * sigma**2.0)
-            exp_term = np.exp(-(x**2 + y**2)/(2 * sigma**2))
-            gaussian_filter[x+size_x, y+size_y] = normal*exp_term
-
-    '''or'''
-
-    # x, y = np.mgrid[-size_y:size_y+1, -size_x:size_x+1]
-    # normal = 1 / (2.0 * np.pi * sigma**2)
-    # gaussian_filter = np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
+            exp_term = np.exp(-(x**2.0 + y**2.0) / (2.0 * sigma**2.0))
+            gaussian_filter[y+m_half, x+n_half] = normal * exp_term
 
     blurred = np.zeros(img.shape, dtype=np.float32)
     blurred = convolution(img, gaussian_filter)
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     from PIL import Image
 
     img = np.array(Image.open('../../assets/lenna.png'))
-    g_filter, blur_image = GuassianBlur(img, 5)
+    g_filter, blur_image = GuassianBlur(img, 5, (40, 40))
 
     # plotting
     plt.subplot(121)
