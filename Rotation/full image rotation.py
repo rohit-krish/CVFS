be4e9df928623512eb65a91e967ffe0a66e38c6d
Rohit <dev.rohitnp@gmail.com>
2023-05-17 10:42:35 +0530

    Modified

--- Rotation/full image rotation.py
+++ Rotation/full image rotation.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import math
 
 
 def naive_image_rotate(image, degree):
@@ -8,26 +7,24 @@ def naive_image_rotate(image, degree):
     This function rotates the image around its center by amount of degrees
     provided. The rotated image will show the full image.
     '''
-    rads = math.radians(degree)
-    # In this case, we consider the rotated image to be different than input
-    # image size which will depend on the rotation angle.
+    rads = np.radians(degree)
+    h, w, c = image.shape
 
-    height_rot_img = round(abs(image.shape[0]*math.cos(rads))) + round(abs(image.shape[1]*math.sin(rads)))
-    width_rot_img = round(abs(image.shape[0]*math.sin(rads))) + round(abs(image.shape[1]*math.cos(rads)))
+    height_rot_img = round(abs(w*np.cos(rads) + h*np.sin(rads)))
+    width_rot_img  = round(abs(h*np.sin(rads) + w*np.cos(rads)))
 
-    rot_img = np.uint8(
-        np.zeros((height_rot_img, width_rot_img, image.shape[2])))
+    rot_img = np.uint8(np.zeros((height_rot_img, width_rot_img, c)))
 
     # Finding the center point of the original image
-    cx, cy = (image.shape[1]//2, image.shape[0]//2)
+    cx, cy = w//2, h//2
 
     # Finding the center point of rotated image.
     midx, midy = (width_rot_img//2, height_rot_img//2)
 
     for i in range(rot_img.shape[0]):
         for j in range(rot_img.shape[1]):
-            x = (i-midx)*math.cos(rads)+(j-midy)*math.sin(rads)
-            y = -(i-midx)*math.sin(rads)+(j-midy)*math.cos(rads)
+            x = (i-midx)*np.cos(rads)+(j-midy)*np.sin(rads)
+            y = -(i-midx)*np.sin(rads)+(j-midy)*np.cos(rads)
 
             x = round(x)+cy
             y = round(y)+cx
